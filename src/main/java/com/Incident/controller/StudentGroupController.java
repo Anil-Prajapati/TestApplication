f2186494a9a71ef946be1f90d9059cae6bcc027b
Anil-Prajapati <anilprajapati80814895@gmail.com>
2024-10-16 18:55:32 +0530

    ch

--- src/main/java/com/Incident/controller/StudentGroupController.java
+++ src/main/java/com/Incident/controller/StudentGroupController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ public class StudentGroupController {
 		this.studentcontroller = studentcontroller;
 	}
 	
+	@GetMapping("/byCode")
 	public Map<String, List<Student>> groupByCource(){
 		List<Student> students = studentcontroller.getAll();
 		return students.stream().collect(Collectors.groupingBy(Student::getCource));
